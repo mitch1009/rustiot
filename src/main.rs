2bf41486eab749e587f62eb3a12e4ff9cacad1ce
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-20 12:39:42 +0200

    feat: add simple game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-    println!("Welcome to rustiot!, your only one stop IoT solution in Africa");
+    println!("simple rustiot Game!");
+    let num = rand::thread_rng().gen_range(1..=100); // Generate a random number
+    loop {
+        println!("Guess A number");
+        let mut input = String::new(); // Create a mutable string
+        io::stdin().read_line(&mut input).unwrap(); // Read user input
+        let input :i32 = input.trim().parse().unwrap();
+        println!("You guessed: {}", input);
+        match input.cmp(&num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
